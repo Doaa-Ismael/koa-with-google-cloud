e83e132750de9f8452f48df5b90675948077c7d2
Doaa <doaa@Doaas-MacBook-Pro.local>
2023-05-07 02:54:40 +0200

    Try Koa-subdomain package

--- app/app.js
+++ app/app.js
@@ -2,13 +2,15 @@
 
 const Koa = require('koa');
 const logging = require('@kasa/koa-logging');
+const Subdomain = require('koa-subdomain');
+const Router = require('koa-router');
 const requestId = require('@kasa/koa-request-id');
 const bodyParser = require('./middlewares/body-parser');
 const cors = require('./middlewares/cors');
 const errorHandler = require('./middlewares/error-handler');
 const corsConfig = require('./config/cors');
 const logger = require('./logger');
-const router = require('./routes');
+const defaultRouter = require('./routes');
 
 
 class App extends Koa {
@@ -50,9 +52,24 @@ class App extends Koa {
   }
 
   _configureRoutes() {
+    const subdomain = new Subdomain();
+    const newRouter = new Router();
+    newRouter.get('/', (ctx) => ctx.body = 'hello!');
+    newRouter.get('/2', (ctx) => ctx.body = 'hello again!');
+    const globalRouter = new Router();
+    globalRouter.get('/', (ctx) => ctx.body = JSON.stringify(ctx.state.wildcardSubdomains));
+
+
+    subdomain.use('one', defaultRouter.routes());
+    subdomain.use('dalia', newRouter.routes());
+    subdomain.use('*', globalRouter.routes());
+    subdomain.use('*.*', globalRouter.routes());
+
+    this.use(subdomain.routes());
+
     // Bootstrap application router
-    this.use(router.routes());
-    this.use(router.allowedMethods());
+    // this.use(defaultRouter.routes());
+    // this.use(defaultRouter.allowedMethods());
   }
 
   listen(...args) {
